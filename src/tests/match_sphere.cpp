96632c8f2908b2a7990b70267d9b44553b679da3
Aaron Goldblum <aarongoldblum2016@u.northwestern.edu>
2014-12-02 00:24:01 -0600

    Match_sphere code woorking without point display

--- src/tests/match_sphere.cpp
+++ src/tests/match_sphere.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
+#include <stdio.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/nonfree/features2d.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <panoramic/Panoramic.hpp>
 #include <iostream>
@@ -24,8 +27,8 @@ int main(int argc, char** argv)
   std::cout << std::endl;
   
   std::cout << "Reading images.\n";
-  cv::Mat first_sample = cv::imread(first_input_path.c_str());
-  cv::Mat second_sample = cv::imread(second_input_path.c_str());
+  cv::Mat first_sample = cv::imread(first_path.c_str());
+  cv::Mat second_sample = cv::imread(second_path.c_str());
 
   nurc::Panoramic p_server;
   cv::Mat first_mask, second_mask;
@@ -38,7 +41,76 @@ int main(int argc, char** argv)
   cv::imshow("Second Warped", second_warped);
 
   // Modify for FLANN code
-  cv::imwrite(output_path.c_str(), warped);
+  using namespace cv;
+
+  Mat img_1 = first_warped;
+  Mat img_2 = second_warped;
+
+  if( !img_1.data || !img_2.data )
+  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+
+  //-- Step 1: Detect the keypoints using SURF Detector
+  int minHessian = 400;
+
+  SurfFeatureDetector detector( minHessian );
+
+  std::vector<KeyPoint> keypoints_1, keypoints_2;
+
+  detector.detect( img_1, keypoints_1, first_mask );
+  detector.detect( img_2, keypoints_2, second_mask );
+
+  //-- Step 2: Calculate descriptors (feature vectors)
+  SurfDescriptorExtractor extractor;
+
+  Mat descriptors_1, descriptors_2;
+
+  extractor.compute( img_1, keypoints_1, descriptors_1 );
+  extractor.compute( img_2, keypoints_2, descriptors_2 );
+
+  //-- Step 3: Matching descriptor vectors using FLANN matcher
+  FlannBasedMatcher matcher;
+  std::vector< DMatch > matches;
+  matcher.match( descriptors_1, descriptors_2, matches );
+
+  double max_dist = 0; double min_dist = 100;
+
+  //-- Quick calculation of max and min distances between keypoints
+  for( int i = 0; i < descriptors_1.rows; i++ )
+  { double dist = matches[i].distance;
+    if( dist < min_dist ) min_dist = dist;
+    if( dist > max_dist ) max_dist = dist;
+  }
+
+  printf("-- Max dist : %f \n", max_dist );
+  printf("-- Min dist : %f \n", min_dist );
+
+  //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+  //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+  //-- small)
+  //-- PS.- radiusMatch can also be used here.
+  std::vector< DMatch > good_matches;
+
+  for( int i = 0; i < descriptors_1.rows; i++ )
+  { if( matches[i].distance <= max(2*min_dist, 0.02) )
+    { good_matches.push_back( matches[i]); }
+  }
+
+  //-- Draw only "good" matches
+  Mat img_matches;
+  drawMatches( img_1, keypoints_1, img_2, keypoints_2,
+               good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+               vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+  //-- Show detected matches
+  imshow( "Good Matches", img_matches );
+
+  for( int i = 0; i < (int)good_matches.size(); i++ )
+  { printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx ); }
+
+  for( int i = 0; i < (int)good_matches.size(); i++ )
+  { printf( "-- Good Match Points [%d] 1 X-value: %d  1 Y-value: %d -- 2 X-value: %d 2 Y-value: %d \n", i, keypoints_1[good_matches[i].queryIdx].pt.x, keypoints_1[good_matches[i].queryIdx].pt.y, keypoints_2[good_matches[i].trainIdx].pt.x, keypoints_2[good_matches[i].trainIdx].pt.y ); }
+
+  cv::imwrite(output_path.c_str(), img_matches);
   
   cv::waitKey(0);
   
