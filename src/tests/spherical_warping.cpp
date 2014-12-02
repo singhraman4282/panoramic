565105fa83a2b9d328f8574fd94f54583e9d21ae
Tigist Diriba <tgdiriba@gmail.com>
2014-12-01 20:27:51 -0600

    focal length input allowed

--- src/tests/spherical_warping.cpp
+++ src/tests/spherical_warping.cpp
@@ -8,18 +8,24 @@
 
 int main(int argc, char** argv)
 {
+  ros::init(argc, argv, "spherical_warping");
+  ros::NodeHandle nh;
+
+  double focal_length;
   std::string input_path, output_path;
   std::cout << "Please input the path to the image: ";
   std::cin >> input_path;
   std::cout << "Please input the path to where to save the warped image: ";
   std::cin >> output_path;
+  std::cout << "Please input the desired focal length: ";
+  std::cin >> focal_length;
   std::cout << std::endl;
   
   std::cout << "Reading image.\n";
   cv::Mat sample = cv::imread(input_path.c_str());
   
   nurc::Panoramic p_server;
-  cv::Mat warped = p_server.map_to_sphere(sample, 1000, 1000, 1500);
+  cv::Mat warped = p_server.map_to_sphere(sample, 1000, 1000, focal_length);
   
   cv::imshow(input_path.c_str(), sample);
   cv::imshow("Warped", warped);
