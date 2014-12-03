7495430039249e9626a0e2a36c4dc18e8bfa417e
Tigist Diriba <tgdiriba@gmail.com>
2014-12-02 21:41:43 -0600

    added ransac test code

---
+++ src/tests/ransac_test.cpp
@@ -0,0 +1,54 @@
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <std_msgs/Header.h>
+#include <panoramic/SphericalStitch.h>
+#include <iostream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ransac_test");
+  ros::NodeHandle nh;
+  
+  std::stringstream ss;
+  std::string p_path = ros::package::getPath("panoramic");
+  std::string first_path, second_path;
+  ss << p_path << "res/images/classroom_test_1.jpg";
+  
+  first_path = ss.str();
+  ss.clear();
+  ss << p_path << "res/images/classroom_test_2.jpg";
+  second_path = ss.str();
+  ss.clear();
+  
+  panoramic::SphericalStitch test_stitch;
+  test_stitch.request.phi_res = test_stitch.request.theta_res = panoramic::SphericalStitchRequest::MAX_RES;
+  test_stitch.request.s = panoramic::SphericalStitchRequest::FOCAL_LENGTH;
+  
+  cv::Mat first_image = cv::imread( first_path.c_str() );
+  cv::Mat second_image = cv::imread( second_path.c_str() );
+  
+  sensor_msgs::Image first_im, second_im;
+  cv_bridge::CvImage cvi_first( std_msgs::Header(), std::string("brg8"), first_image );
+  cv_bridge::CvImage cvi_second( std_msgs::Header(), std::string("brg8"), second_image );
+  cvi_first.toImageMsg( first_im );
+  cvi_second.toImageMsg( second_im ); 
+  
+  test_stitch.request.queue.push_back( first_im );
+  test_stitch.request.queue.push_back( second_im );
+  
+  ros::ServiceClient p_client = nh.serviceClient<panoramic::SphericalStitch>("stitch");
+  if(p_client.call( test_stitch )) {
+    ROS_INFO("Successfully called the service.");
+  }
+  else {
+    ROS_ERROR("Could not call the panoramic server.");
+  }
+
+  ros::spin();
+
+}
