e8f306d26e907b7515d9084b929001451f61a653
Tigist Diriba <tgdiriba@gmail.com>
2014-12-04 11:24:56 -0600

    masks

--- src/tests/FLANN.cpp
+++ src/tests/FLANN.cpp
@@ -20,10 +20,10 @@ int main( int argc, char** argv )
 
   Mat img_1 = imread( argv[1]);
   Mat mask_1 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
-  Mat img_2 = imread( argv[3]);
+  Mat img_2 = imread( argv[3] );
   Mat mask_2 = imread( argv[4], CV_LOAD_IMAGE_GRAYSCALE );
 
-  if( !img_1.data || !img_2.data )
+  if( !img_1.data || !img_2.data || !mask_1.data || !mask_2.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
 
   //-- Step 1: Detect the keypoints using SURF Detector
